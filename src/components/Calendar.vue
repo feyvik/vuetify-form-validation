9d71a28c86a6722dea8d9e98c52cb91a1a408fc9
favour vivian woka <vivianemma97@gmail.com>
2020-08-06 03:41:45 +0100

    set up project

---
+++ src/components/Calendar.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    <v-carousel hide-delimiters>
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+    ></v-carousel-item>
+  </v-carousel>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Calender',
+
+    data () {
+      return {
+        items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+          }
+        ]
+      }
+    },
+  }
+</script>
