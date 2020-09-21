c172e44a90da36ec68505e53a64cbfb71e2f2f48
favour vivian woka <vivianemma97@gmail.com>
2020-09-21 23:24:39 +0100

    set validation errors on name input, email input, password input and checkbox input

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -31,7 +31,7 @@
             <v-col cols="6">
               <v-text-field
                 v-model="firstname"
-                :rules="firstnameRules"
+                :rules="nameRules"
                 label="First Name"
                 required
               ></v-text-field>
@@ -39,7 +39,7 @@
             <v-col cols="6">
               <v-text-field
                 v-model="lastname"
-                :rules="lastnameRules"
+                :rules="nameRules"
                 label="Last Name"
                 required
               ></v-text-field>
@@ -99,6 +99,24 @@
 <script>
 export default {
   name: "RegisterForm",
+  data: () => ({
+    name: '',
+    nameRules: [
+      v => !!v || 'Name is required',
+      v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+    ],
+    email: '',
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()\\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v) || 'E-mail must be valid',
+    ],
+    password: '',
+    passwordRules: [
+      v => !!v || 'Password is required',
+      v => /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/.test(v) || 'Password must contain at least lowercase letter, one number, a special character and one uppercase letter',
+    ],
+    checkbox: false,
+  }),
 };
 </script>
 <style scoped>
@@ -106,9 +124,4 @@ export default {
   margin: 0;
   padding: 0;
 }
-/* .row {
-  height: 100vh;
-  margin: 0;
-  padding: 0;
-} */
 </style>
