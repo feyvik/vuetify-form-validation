09a925031fea1bc4fda110937407ea727219c456
favour vivian woka <vivianemma97@gmail.com>
2020-09-21 16:48:52 +0100

    build vuetify form validation ui

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,12 +1,114 @@
 <template>
-  <v-container>
-    
+  <v-container fluid class="fluid">
+    <v-row justify="center" align="center" class="row">
+      <v-col
+        cols="6"
+        sm="12"
+        md="6"
+        xs="12"
+        class="text-center purple darken-2"
+        style="height: 100vh"
+      >
+        <v-row justify="center" align="center" style="height: 80vh">
+          <v-col>
+            <h1 class="white--text">LogRockets</h1>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col
+        cols="6"
+        sm="12"
+        md="6"
+        xs="12"
+        class="text-center"
+        style="height: 100vh"
+      >
+        <h1>Welcome to LogRockets</h1>
+        <p>Please complete this form to create an account</p>
+
+        <v-form ref="form" class="mx-2" v-model="valid" lazy-validation>
+          <v-row>
+            <v-col cols="6">
+              <v-text-field
+                v-model="firstname"
+                :rules="firstnameRules"
+                label="First Name"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                v-model="lastname"
+                :rules="lastnameRules"
+                label="Last Name"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                v-model="email"
+                :rules="emailRules"
+                label="Email"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="6">
+              <v-text-field
+                v-model="password"
+                :rules="passwordRules"
+                label="Password"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                v-model="retypepassword"
+                :rules="passwordRules"
+                label="Re-type Password"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+         
+          <v-checkbox
+            v-model="checkbox"
+            :rules="[v => !!v || 'You must agree to continue!']"
+            label="I agree with Terms and Conditions"
+            required
+          ></v-checkbox>
+        
+          <v-checkbox
+            v-model="checkbox"
+            :rules="[v => !!v || 'You must agree to receive!']"
+            label="I want to receive LogRocket Emails"
+            required
+          ></v-checkbox>
+          <v-btn class="purple darken-2 white--text mt-5"  @click="submitForm"> 
+            Register
+          </v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-  export default {
-    name: 'RegisterForm',
-
-  } 
+export default {
+  name: "RegisterForm",
+};
 </script>
+<style scoped>
+.fluid {
+  margin: 0;
+  padding: 0;
+}
+/* .row {
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+} */
+</style>
